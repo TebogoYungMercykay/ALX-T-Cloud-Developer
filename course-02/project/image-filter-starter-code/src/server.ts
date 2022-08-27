058f310a99eb611593e259ae2e46492b91b8e55f
Selepe Sello Tebogo <91455209+TebogoYungMercykay@users.noreply.github.com>
2022-08-27 18:36:11 +0200

    Update server.ts

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import {Router, Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -6,7 +7,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Init the Express application
   const app = express();
-
   // Set the network port
   const port = process.env.PORT || 8082;
   
@@ -28,7 +28,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-
+  app.get('/filteredimage', async(req: Request, req: Response)=>{
+    const image_url = req.query.image_url.toString();
+    if(!image_url){
+      res.status(400).send('Image url is required'); 
+    }
+    const filtered_image = await filterImageFromURL(image_url);
+    res.status(200).sendFile(filtered_image, ()=>{
+      deleteLocalFiles([filtered_image]);
+    });
+  });
   //! END @TODO1
   
   // Root Endpoint
